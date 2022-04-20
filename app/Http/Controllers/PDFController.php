2db0eb65b6dbf8a2b5e80167c37df299698b8f5c
fakhrulzulfi <fakhrulzulfi@gmail.com>
2022-04-20 20:16:31 +0700

    add file manual instructions PDF

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+class PDFController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($name)
+    {
+        $nameOfFile = "$name.pdf";
+        $file = public_path().DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.$nameOfFile;
+        $file = File::get($file);
+        $response = Response::make($file,200);
+        $response->header('Content-Type', 'application/pdf');
+        return $response;
+    }
+}
